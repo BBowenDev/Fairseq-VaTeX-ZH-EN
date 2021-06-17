b35421be34dcbb362e0413f99e0ab848733e44a8
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-06-17 15:34:33 -0400

    Delete src directory

--- src/mmt/__init__.py
+++
@@ -1,4 +0,0 @@
-from .tasks.multimodal_translation import MultimodalTranslationTask
-from .models.multimodal_transformer import MultimodalTransformerModel
-from .models.imgc_transformer import ImgCTransformerModel
-from .models.imgatt_transformer import ImgAttTransformerModel
