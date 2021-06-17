596cb8c4a050956b2f8b71dc30702fed3a3cc1e4
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-06-17 14:11:37 -0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -10,21 +10,17 @@ mkdir -p $FV/external/fairseq
 mkdir -p $FV/external/apex
 	
 #install Fairseq
+echo "Installing Fairseq"
 cd $FV/external/fairseq
 git clone https://github.com/pytorch/fairseq
 cd fairseq
 git submodule update --init --recursive	
 pip install --editable ./
 
-#install Apex
-cd $FV/
-git clone https://github.com/NVIDIA/apex
-cd apex
-pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
-
 if [ ! -d "${FV}/subword-nmt" ]
 then
 	#install subword-nmt
+	echo "Installing subword-nmt"
 	cd $FV
 	git clone https://github.com/rsennrich/subword-nmt
 	cd subword-nmt
