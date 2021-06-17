490ca0e2eb298fbb1f57fb29fb0b57284e12be6f
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-06-17 15:51:29 -0400

    Add files via upload

---
+++ prepare.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Installing Prerequisites"
+
+pip install nltk
+pip install jieba
+
+FV=$(pwd)
+
+if [ ! -d "${FV}/vatex" ]
+then
+	#create vatex folders
+	mkdir -p $FV/vatex/scripts
+fi
+
+mv vatex_preprocess.py $FV/vatex/scripts
+
+if [ ! -d "${FV}/external" ]
+then 
+	#create missing directories
+	mkdir $FV/external
+	
+	#install fairseq
+	echo "Installing Fairseq"
+	cd $FV/external
+	git clone https://github.com/pytorch/fairseq
+	cd fairseq
+	git submodule update --init --recursive
+	pip install --editable ./
+fi
+
+if [ ! -d "${FV}/subword-nmt" ]
+then
+	#install subword-nmt
+	cd $FV
+	git clone https://github.com/rsennrich/subword-nmt
+fi
