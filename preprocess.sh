a0a6efa40b0a4786fb88f18cc8b4ddf259e4c352
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-06-17 15:26:43 -0400

    Add files via upload

---
+++ preprocess.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+FV=$(pwd)
+VT=$FV/vatex
+
+#format vatex folder
+if [ ! -d "${VATEX}/raw" ]
+then 
+	mkdir $VATEX/raw
+fi
+
+if [ ! -d "${VATEX}/tok" ]
+then
+	mkdir $VATEX/tok
+fi
+
+if [ ! -d "${VATEX}/bpe" ]
+then
+	mkdir $VATEX/bpe
+fi
+
+TOK=$VATEX/tok
+RAW=$VATEX/raw
+BPE=$VATEX/bpe
+
+#get raw captions
+wget https://eric-xw.github.io/vatex-website/data/vatex_training_v1.0.json $RAW
+wget https://eric-xw.github.io/vatex-website/data/vatex_validation_v1.0.json $RAW
+wget https://eric-xw.github.io/vatex-website/data/vatex_public_test_english_v1.1.json $RAW
+
+#run preprocessing script on raw captions, tokenizing and saving to new files
+cd $VATEX/scripts
+python vatex_preprocess.py
+
+cd $FV/subword-nmt
+
+#10,000 merge operations are used [can be hyperparamaterized] 
+
+#process is first completed in English
+python ./learn_bpe.py -s 10000 < "${TOK}/train_tok.en" > "${TOK}/codes_en.bpe"
+python ./apply_bpe.py -c "${TOK}/codes_en.bpe" < "${TOK}/train_tok.en" > "${BPE}/train.bpe10000.en
+
+#process is repeated with Chinese
+python ./learn_bpe.py -s 10000 < "${TOK}/train_tok.zh" > "${TOK}/codes_zh.bpe"
+python ./apply_bpe.py -c "${TOK}/codes_zh.bpe" < "${TOK}/train_tok.zh" > "${BPE}/train.bpe10000.zh
