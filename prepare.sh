f8c24640adb1f0c8719c640ce7f21b22ebb95c50
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-06-17 15:03:16 -0400

    Add files via upload

---
+++ prepare.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Installing Prerequisites"
+
+pip install nltk
+pip install jieba
+
+FV=$(pwd)
+
+if [ ! -d "${FV}/external" ]
+then 
+	#create missing directories
+	mkdir $FV/external
+	
+	#install fairseq
+	echo "Installing Fairseq"
+	cd $FV/external
+	git clone https://github.com/pytorch/fairseq
+	cd fairseq
+	git submodule update --init --recursive
+	pip install --editable ./
+fi
+
+if [ ! -d "${FV}/subword-nmt" ]
+then
+	#install subword-nmt
+	cd $FV
+	git clone https://github.com/rsennrich/subword-nmt
+fi
