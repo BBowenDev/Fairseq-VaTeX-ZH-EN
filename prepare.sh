9c22421a64a1fc6ca84eb0bf64cdc07d479fbc6d
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-06-17 14:24:30 -0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-
 echo "Installing prerequisite packages"
 pip install nltk 
 pip install jieba
-
 FV=$(pwd)
-
 mkdir -p $FV/external/fairseq
 mkdir -p $FV/external/apex
-
 #install fairseq
 echo "Installing Fairseq"
 cd $FV/external/fairseq
 git clone https://github/com/pytorch/fairseq
 cd fairseq
 git submodule update --init --recursive
-
 if [ ! -d "${FV}/subowrd-nmt" ]
 then 
 	#install subword-nmt
