2b3463e60afaa94ad25a46fd51bbd69f32228c22
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-06-17 10:28:17 -0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,7 @@
 pip install nltk
 pip install jieba
 FV=$(pwd)
+echo "Checking External"
 if [ ! -d $FV/external ]
 then
 	mkdir $FV/external
@@ -13,15 +14,18 @@ then
 	cd fairseq
 	git submodule update --init --recursive	
 	pip install --editable ./
+	echo "Installed Fairseq"
 	#install Apex
 	git clone https://github.com/NVIDIA/apex
 	cd apex
 	pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+	echo "Installed Apex"
 fi
+echo "Checking subword-nmt"
 if [ ! -d $FV/subword-nmt ]
-then
+then		
 	#install subword-nmt
 	cd $FV
 	git clone https://github.com/rsennrich/subword-nmt
+	echo "Installed subword-nmt"
 fi
-exit 0
