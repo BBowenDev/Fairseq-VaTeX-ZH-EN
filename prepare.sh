441348140ae7130fcfdda9eb0a6a8f963b67f639
Braeden <81532750+BraedenLB@users.noreply.github.com>
2021-06-21 10:20:26 -0400

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -7,8 +7,7 @@ pip install jieba
 
 FV=$(pwd)
 
-if [ ! -d "${FV}/vatex" ]
-then
+if [ ! -d "${FV}/vatex" ]; then
 	#create vatex folders
 	mkdir $FV/vatex
 	mkdir $FV/vatex/scripts
@@ -16,8 +15,7 @@ fi
 
 mv vatex_preprocess.py $FV/vatex/scripts
 
-if [ ! -d "${FV}/external" ]
-then 
+if [ ! -d "${FV}/external" ]; then 
 	#create missing directories
 	mkdir $FV/external
 	
@@ -30,8 +28,7 @@ then
 	pip install --editable ./
 fi
 
-if [ ! -d "${FV}/subword-nmt" ]
-then
+if [ ! -d "${FV}/subword-nmt" ]; then
 	#install subword-nmt
 	cd $FV
 	git clone https://github.com/rsennrich/subword-nmt
